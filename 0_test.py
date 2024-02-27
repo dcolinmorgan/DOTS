7a0dd00b82be1c4b4a20425828aaa744048cc752
Daniel <dcolinmorgan@gmail.com>
2024-02-27 18:32:46 +0800

    first few models trained

---
+++ 0_test.py
@@ -0,0 +1,80 @@
+## from https://huggingface.co/docs/transformers/training
+# !pip install torch torchvision torchaudio
+# !pip install datasets transformers --upgrade
+# !pip3 install evaluate
+# Import libraries
+import os,sys,torch,evaluate, numpy as np
+from datasets import load_dataset
+from sklearn.preprocessing import LabelEncoder
+from transformers import AutoTokenizer, AutoModelForMaskedLM, AutoModelForSequenceClassification, TrainingArguments, Trainer
+sys.path.append(os.getcwd()) # Add current directory to PYTHONPATH if running as script
+from sklearn.decomposition import PCA 
+from sklearn.cluster import KMeans
+from cu_cat import TableVectorizer
+
+# datasetA = load_dataset("yelp_review_full")
+device = torch.device('mps')
+dataset = load_dataset("melisekm/natural-disasters-from-social-media")
+
+model_name = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForMaskedLM.from_pretrained(model_name).to(device)
+
+# from cu_cat._dep_manager import deps
+# deps.cuml=None
+# AA = TableVectorizer().fit_transform(dataset['train']['label'])
+# pca = PCA(2)
+# pca_data = pd.DataFrame(pca.fit_transform(AA),columns=['PC1','PC2'])
+# kmeans =KMeans(n_clusters=2).fit(AA)
+# pca_data['cluster'] = pd.Categorical(kmeans.labels_)
+# fig,ax = plt.subplots()
+# scatter = ax.scatter(pca_data['PC1'], pca_data['PC2'],c=pca_data['cluster'],cmap='Set3',alpha=0.7)
+# legend1 = ax.legend(*scatter.legend_elements(),
+#                     loc="upper left", title="")
+# ax.add_artist(legend1)
+# plt.savefig('test')
+
+dataset = dataset.map(lambda examples: {'label': TableVectorizer().fit_transform(examples['label'])}, batched=True)
+dataset = dataset.map(lambda examples: {'label': KMeans(n_clusters=2).fit(examples['label']).labels_}, batched=True)
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(50000)).map(lambda examples:{'text': examples['text'], 'label': examples['label']},remove_columns=['target', 'SOURCE_FILE', 'tweet_id', 'filename', 'event_type', 'event_type_detail'])
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(10000)).map(lambda examples:{'text': examples['text'], 'label': examples['label']},remove_columns=['target', 'SOURCE_FILE', 'tweet_id', 'filename', 'event_type', 'event_type_detail'])
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=5).to(device)
+training_args = TrainingArguments(output_dir="test_trainer")
+
+metric = evaluate.load("accuracy")
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch",save_strategy="no")
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset= small_train_dataset,
+    eval_dataset= small_eval_dataset,
+    compute_metrics=compute_metrics,
+)
+config = {"repo_id": 'Dcolinmorgan'}
+
+trainer.train("test_trainer/checkpoint-13500")
+
+
+# save locally
+model.save_pretrained("disaster-mlx-model", config=config)
+
+# push to the hub
+model.push_to_hub("disaster-mlx-model")#, config=config)
+
+model = model.from_pretrained("disaster-mlx-model")
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained('disaster-mlx-model')
