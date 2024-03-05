b89dd9e45333602967d90f0f49650c3facc6d904
Daniel <dcolinmorgan@gmail.com>
2024-03-05 12:32:13 +0800

    copy setup

---
+++ mlx_grph/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+def unique_flatten_dict(d):
+  return list(set(sum( d.values(), [] )))
+
+core_requires = [
+  'spacy',
+  'bs4',
+  'sklearn',
+  'transformers',
+  'requests',
+  'xml',
+  'nltk',
+  'string',
+  'numpy',
+  'setuptools'
+]
+
+
+setup(
+    name='mlx_grph',
+    version='0.0.1',
+    packages = find_packages(),
+    platforms='any',
+    python_requires='>=3.7',
+    install_requires=core_requires,
+    license='BSD',
+    classifiers=[
+        'Development Status :: 0 - Fun',
+    ],
+    keywords=['news cpu featurizer']
+)
