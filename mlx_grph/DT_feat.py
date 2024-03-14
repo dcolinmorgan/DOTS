05ee8bc88d591152ac2765cb8460b697fb2361bc
Daniel <dcolinmorgan@gmail.com>
2024-03-14 18:52:03 +0800

    moved key to token

--- mlx_grph/DT_feat.py
+++ mlx_grph/DT_feat.py
@@ -41,58 +41,8 @@ embeddings=[]
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
+
 # Define functions
-def chunk_text(text, max_len):
-    tokens = nltk.word_tokenize(text)
-    num_chunks = len(tokens) // max_len
-    final_chunk_size = len(tokens) % max_len
-    logging.info(f"chunking artcile into {num_chunks} chunks")
-    # If the final chunk is too small, distribute its tokens among the other chunks
-    if final_chunk_size < max_len / 2:
-        num_chunks += 1
-        chunk_sizes = [len(tokens) // num_chunks + (1 if i < len(tokens) % num_chunks else 0) for i in range(num_chunks)]
-        chunks = [tokens[sum(chunk_sizes[:i]):sum(chunk_sizes[:i+1])] for i in range(num_chunks)]
-    else:
-        chunks = [tokens[i:i + max_len] for i in range(0, len(tokens), max_len)]
-    return chunks
-
-
-def featurize_stories(text, top_k, max_len):
-    # Extract candidate words/phrases
-    count = CountVectorizer(ngram_range=n_gram_range, stop_words=stop_words).fit([text])
-    all_candidates = count.get_feature_names_out()
-    doc = nlp(text)
-    noun_phrases = set(chunk.text.strip().lower() for chunk in doc.noun_chunks)
-    nouns = set()
-    datetimes = set()
-    for token in doc:
-        if token.pos_ == "NOUN":
-            nouns.add(token.text)
-        if token.ent_type_ == "DATE":
-            datetimes.add(token.text)  # no need to tokenize dates, just add to feature list artificially by repeating
-    all_nouns = nouns.union(noun_phrases)
-    candidates = list(filter(lambda candidate: candidate in all_nouns, all_candidates))
-    candidate_tokens = tokenizer(candidates, padding=True, return_tensors="pt")
-    if device == 'cuda':
-        candidate_tokens = {k: v.to(device) for k, v in (candidate_tokens).items()}
-    candidate_embeddings = model(**candidate_tokens)["pooler_output"]
-    candidate_embeddings = candidate_embeddings.detach()  # .to_numpy
-    chunks = chunk_text(text, max_len)  # use this to chunk better and use less padding thus less memory but also less affect from averging
-    for chunk in chunks:
-        text_tokens = tokenizer(chunk, padding=True, return_tensors="pt")
-        if device == 'cuda':
-            text_tokens = {k: v.to(device) for k, v in (text_tokens).items()}
-        text_embedding = model(**text_tokens)["pooler_output"]
-        text_embedding = text_embedding.detach()#.to_numpy()
-        embeddings.append(text_embedding)
-    max_emb_shape = max(embedding.shape[0] for embedding in embeddings)
-    padded_embeddings = [np.pad(embedding.cpu(), ((0, max_emb_shape - embedding.shape[0]), (0, 0))) for embedding in embeddings]
-    avg_embedding = np.min(padded_embeddings, axis=0)
-    distances = cosine_similarity(avg_embedding, candidate_embeddings.cpu())
-    torch.cuda.empty_cache()
-    return [candidates[index] for index in distances.argsort()[0][::-1][-top_k:]]
-
-
 def get_data(n=args.n):
     bash_command = f"""
     curl -X GET "{os_url}" -H 'Content-Type: application/json' -d '{{
@@ -160,6 +110,58 @@ def process_data(data,fast=args.s):
             writer.writerow([loc,date,text]) # force location into top feature, also assume title has important info
             writer.writerow(['\n'])
     return articles
+
+
+def chunk_text(text, max_len):
+    tokens = nltk.word_tokenize(text)
+    num_chunks = len(tokens) // max_len
+    final_chunk_size = len(tokens) % max_len
+    logging.info(f"chunking artcile into {num_chunks} chunks")
+    # If the final chunk is too small, distribute its tokens among the other chunks
+    if final_chunk_size < max_len / 2:
+        num_chunks += 1
+        chunk_sizes = [len(tokens) // num_chunks + (1 if i < len(tokens) % num_chunks else 0) for i in range(num_chunks)]
+        chunks = [tokens[sum(chunk_sizes[:i]):sum(chunk_sizes[:i+1])] for i in range(num_chunks)]
+    else:
+        chunks = [tokens[i:i + max_len] for i in range(0, len(tokens), max_len)]
+    return chunks
+
+
+def featurize_stories(text, top_k, max_len):
+    # Extract candidate words/phrases
+    count = CountVectorizer(ngram_range=n_gram_range, stop_words=stop_words).fit([text])
+    all_candidates = count.get_feature_names_out()
+    doc = nlp(text)
+    noun_phrases = set(chunk.text.strip().lower() for chunk in doc.noun_chunks)
+    nouns = set()
+    datetimes = set()
+    for token in doc:
+        if token.pos_ == "NOUN":
+            nouns.add(token.text)
+        if token.ent_type_ == "DATE":
+            datetimes.add(token.text)  # no need to tokenize dates, just add to feature list artificially by repeating
+    all_nouns = nouns.union(noun_phrases)
+    candidates = list(filter(lambda candidate: candidate in all_nouns, all_candidates))
+    candidate_tokens = tokenizer(candidates, padding=True, return_tensors="pt")
+    if device == 'cuda':
+        candidate_tokens = {k: v.to(device) for k, v in (candidate_tokens).items()}
+    candidate_embeddings = model(**candidate_tokens)["pooler_output"]
+    candidate_embeddings = candidate_embeddings.detach()  # .to_numpy
+    chunks = chunk_text(text, max_len)  # use this to chunk better and use less padding thus less memory but also less affect from averging
+    for chunk in chunks:
+        text_tokens = tokenizer(chunk, padding=True, return_tensors="pt")
+        if device == 'cuda':
+            text_tokens = {k: v.to(device) for k, v in (text_tokens).items()}
+        text_embedding = model(**text_tokens)["pooler_output"]
+        text_embedding = text_embedding.detach()#.to_numpy()
+        embeddings.append(text_embedding)
+    max_emb_shape = max(embedding.shape[0] for embedding in embeddings)
+    padded_embeddings = [np.pad(embedding.cpu(), ((0, max_emb_shape - embedding.shape[0]), (0, 0))) for embedding in embeddings]
+    avg_embedding = np.min(padded_embeddings, axis=0)
+    distances = cosine_similarity(avg_embedding, candidate_embeddings.cpu())
+    torch.cuda.empty_cache()
+    return [candidates[index] for index in distances.argsort()[0][::-1][-top_k:]]
+
 
 
 # Main pipeline
